39ec0faca94ac38ab6cc5b023747ad3c392839fb
FeldbergerLeizer <goldyilo613@gmail.com>
2021-05-20 21:29:02 -0400

    'newHomework46'

---
+++ 46/Homework46.py
@@ -0,0 +1,18 @@
+import random
+class Die:
+   
+    def __init__(self,sides ):
+        self._sides = sides
+        
+    def roll(self):
+        return random.randint(1,self._sides)
+
+myDie = Die(6)
+print(myDie.roll())
+
+class sixSided(Die):
+    def __init__(self):
+        super().__init__(6)
+
+myDie =sixSided()
+print(myDie.roll())
