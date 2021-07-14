412b7dcb23633d96543cac7759f04a75817a5200
FeldbergerLeizer <goldyilo613@gmail.com>
2021-07-13 20:37:42 -0400

    Homework60

---
+++ 60/one.js
@@ -0,0 +1,4 @@
+'use strict'
+
+var name = 'feldberger';
+var email = 'course@gmail.com'
---
+++ 60/two.js
@@ -0,0 +1,5 @@
+'use strict'
+console.log('My name is '+name+' and my email is '+email);
+
+var age = prompt('Whats your age?');
+console.log('Hello your ' + age +' old');
